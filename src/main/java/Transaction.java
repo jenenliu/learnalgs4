a9eb7298daccccf528d30e780511d5732929e9c8
jenenliu <jenenliu@gmail.com>
2018-05-11 07:30:35 +0800

    add hashCode function to Transaction, and finish ex 3.4.25

--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,3 +1,5 @@
+import org.omg.CORBA.INTERNAL;
+
 import java.awt.font.TransformAttribute;
 import java.util.Comparator;
 import java.util.Date;
@@ -7,6 +9,8 @@ public class Transaction {
     private final Date when;
     private final double amount;
     private int index;
+
+    private Integer myhash;   // store hash value for Transaction
 
     public Transaction(String name, Date trantime, double tranamount, int i)
     {
@@ -60,5 +64,17 @@ public class Transaction {
             if (v.amount == w.amount && v.index > w.index) return 1;
             return 0;
         }
+    }
+
+    public int hashCode() {
+        if (myhash != null) // if we got computed hash value, just return
+            return myhash;
+
+        int hash = 17;
+        hash = 31 * hash + who.hashCode();
+        hash = 31 * hash + when.hashCode();
+        hash = 31 * hash + ((Double) amount).hashCode();
+        myhash = hash;    // store hash value to avoid repeat computation
+        return hash;
     }
 }
