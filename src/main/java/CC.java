cc05c15883c3ce18226bf7cdce3d78b1b0be6798
jenenliu <jenenliu@gmail.com>
2018-05-28 15:31:41 +0800

    add final Graph example code

---
+++ src/main/java/CC.java
@@ -0,0 +1,60 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class CC {
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public CC(Graph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+        for (int s = 0; s < G.V(); s++) {
+            if (!marked[s]) {
+                dfs(G, s);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Graph G, int s) {
+        marked[s] = true;
+        id[s] = count;
+        for (int v : G.adj(s)) {
+            dfs(G, v);
+        }
+    }
+
+    public boolean connected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    public int id(int v) {
+        return id[v];
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        CC cc = new CC(G);
+
+        int M = cc.count();
+        StdOut.println(M + " components");
+
+        Bag<Integer>[] components;
+        components = (Bag<Integer>[]) new Bag[M];
+        for (int i = 0; i < M; i++)
+            components[i] = new Bag<Integer>();
+        for (int v = 0; v < G.V(); v++)
+            components[cc.id(v)].add(v);
+        for (int i = 0; i < M; i++) {
+            for (int v : components[i])
+                StdOut.print(v + " ");
+            StdOut.println();
+        }
+    }
+}
