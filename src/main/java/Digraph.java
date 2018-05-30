ede066f58aba09f3f3ace3964eb84bd3644af1f9
jenenliu <jenenliu@gmail.com>
2018-05-31 07:39:48 +0800

    add first part of Digraph example code

---
+++ src/main/java/Digraph.java
@@ -0,0 +1,48 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class Digraph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Digraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int i = 0; i < V; i++)
+            adj[i] = new Bag<Integer>();
+    }
+
+    public Digraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() { return V; }
+    public int E() { return E; }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public Digraph reverse() {
+        Digraph R = new Digraph(V);
+        for (int v = 0; v < V; v++) {
+            for (int w : adj(v))
+                R.addEdge(w, v);
+        }
+
+        return R;
+    }
+}
