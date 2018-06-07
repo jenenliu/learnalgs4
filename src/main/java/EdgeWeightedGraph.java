bcdd4d1a814328bd67110cba3c4dd6b6cf3c0fe3
jenenliu <jenenliu@gmail.com>
2018-06-07 23:35:53 +0800

    add MST example code

---
+++ src/main/java/EdgeWeightedGraph.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class EdgeWeightedGraph {
+    private final int V;
+    private int E;
+    private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Edge>();
+    }
+
+    public EdgeWeightedGraph(In in) {
+        this.V = in.readInt();
+    }
+
+    public int V() { return V; }
+    public int E() { return E; }
+
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+
+    public Iterable<Edge> edges() {
+        Bag<Edge> b = new Bag<Edge>();
+        for (int v = 0; v < V; v++)
+            for (Edge e : adj[v])
+                if (e.other(v) > v) b.add(e);
+        return b;
+    }
+}
+
---
+++ src/main/java/KruskalMST.java
@@ -0,0 +1,28 @@
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+public class KruskalMST {
+    private Queue<Edge> mst;
+
+    public KruskalMST(EdgeWeightedGraph G) {
+        mst = new Queue<Edge>();
+        MinPQ<Edge> pq = new MinPQ<Edge>(G.E());
+        UF uf = new UF(G.V());
+
+        while (!pq.isEmpty() && mst.size() < G.V()-1) {
+            Edge e = pq.delMin();
+            int v = e.either(), w = e.other(v);
+            if (uf.connected(v,w)) continue;
+            uf.union(v, w);
+            mst.enqueue(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        return 0.0;
+    }
+}
